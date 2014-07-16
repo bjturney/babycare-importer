45f3edb973ab6f35777a53237912100959fe177f
bjturney <bjturney@users.noreply.github.com>
2014-07-15 21:51:45 -0500

    path to outside files
    - Add path vars to location outside project
    - copy db before import
    - import in copy of db

--- importer.js
+++ importer.js
@@ -2,10 +2,21 @@ var parse = require('csv-parse');
 var fs = require('fs');
 var util = require('util');
 var sqlite = require('sqlite3');
+var path = require('path');
 
-var db = new sqlite.Database('BabyCare_updated.db');
+var homepath = process.env.HOME || process.env.HOMEPATH || process.env.USERPROFILE;
 
-fs.readFile('BabyCareData.csv', function(err, data){
+var basePath = path.join(homepath, 'Dropbox','BabyCareApp');
+var CSVPath = path.join(basePath, 'BabyCareData.csv');
+var DBPathOrg = path.join(basePath, 'BabyCare.db');
+var DBPathAlt = path.join(basePath, 'BabyCare-altered.db');
+
+fs.writeFileSync(DBPathAlt, fs.readFileSync(DBPathOrg));
+
+var db = new sqlite.Database(DBPathAlt);
+debugger;
+
+fs.readFile(CSVPath, function(err, data){
 	parse(data.toString(), {columns:true}, function(err, records){
 		var query = db.prepare("INSERT OR REPLACE into event_v2 (eventtype,subtype,amount,duration,_eventid,_babyid,createtime,updatetime,endtime,status,recordstatus,mark,starttime) VALUES (?,?,?,?,?,?,?,?,?,?,?,?,?) ");
 		records.forEach(function(values){
