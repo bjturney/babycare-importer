c14286ebc4dd160cf865a599ae16e3a99aea54b6
bjturney <bjturney@users.noreply.github.com>
2014-07-20 21:55:21 -0500

    node module
    - convert to importer.js to node module
    - apply bug fix for null values

--- importer.js
+++ importer.js
@@ -3,6 +3,7 @@ var fs = require('fs');
 var util = require('util');
 var sqlite = require('sqlite3');
 var path = require('path');
+var _ = require('lodash');
 
 var homepath = process.env.HOME || process.env.HOMEPATH || process.env.USERPROFILE;
 
@@ -11,15 +12,25 @@ var CSVPath = path.join(basePath, 'BabyCareData.csv');
 var DBPathOrg = path.join(basePath, 'BabyCare.db');
 var DBPathAlt = path.join(basePath, 'BabyCare-altered.db');
 
-fs.writeFileSync(DBPathAlt, fs.readFileSync(DBPathOrg));
+var Importer = {
+	run: function() {
+		this.copydb();
+		fs.readFile(CSVPath, this.csvParse);
+	},
+	copydb: function(){
+		fs.writeFileSync(DBPathAlt, fs.readFileSync(DBPathOrg));
+	},
+	csvParse: function(err, data){
+		parse(data.toString(), {columns:true}, this.dbImport);
+	},
+	dbImport: function(err, records){
+		var db = new sqlite.Database(DBPathAlt);
 
-var db = new sqlite.Database(DBPathAlt);
-debugger;
-
-fs.readFile(CSVPath, function(err, data){
-	parse(data.toString(), {columns:true}, function(err, records){
 		var query = db.prepare("INSERT OR REPLACE into event_v2 (eventtype,subtype,amount,duration,_eventid,_babyid,createtime,updatetime,endtime,status,recordstatus,mark,starttime) VALUES (?,?,?,?,?,?,?,?,?,?,?,?,?) ");
 		records.forEach(function(values){
+			values = _.mapValues(values, function(value){
+				return (_.isEmpty(value)) ? null : value;
+			});
 			query.run([
 				values.eventtype,
 				values.subtype,
@@ -36,7 +47,9 @@ fs.readFile(CSVPath, function(err, data){
 				values.starttime
 			]);
 		});
-	});
-});
+	}
+};
+
+module.exports = _.bindAll(Importer);
 
 
